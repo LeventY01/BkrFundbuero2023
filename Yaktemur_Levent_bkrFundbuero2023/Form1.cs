cdeb3fe6e33d66e26db6013e2ab5f71ca368cb03
uMarkiBarki <mstarodubzev@mail.ru>
2023-03-15 09:00:49 +0100

    DBase und SQLConnector added

--- Yaktemur_Levent_bkrFundbuero2023/Form1.cs
+++ Yaktemur_Levent_bkrFundbuero2023/Form1.cs
@@ -6,5 +6,10 @@ namespace Yaktemur_Levent_bkrFundbuero2023
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
